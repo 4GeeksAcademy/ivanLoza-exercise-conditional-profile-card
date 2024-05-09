bea3794afeebf3a335fe615738aee2e15cdfba51
ivanlozaDEV <159062304+ivanlozaDEV@users.noreply.github.com>
2024-05-09 00:31:57 +0000

    added conditionals

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ import "../style/index.css";
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+        avatarURL: "https://randomuser.me/api/portraits/lego/7.jpg, // this is the url for the profile avatar
         socialMediaPosition: "right", // social media bar position (left or right)
         
         twitter: null, // social media usernames
@@ -17,7 +17,7 @@ import "../style/index.css";
 
         name: null,
         lastName: null,
-        role: null,
+        role: programador,
         country: null,
         city: null
     }
@@ -33,14 +33,22 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1> ${variables.name ? variables.name : "Here goes your name"}, ${
+    variables.lastName ? variables.lastName : "and last name"
+  } </h1>
+          <h2>${variables.role ? variables.role : "Your role"}</h2>
+          <h3>${variables.country ? variables.country : "Your country"},${
+    variables.city ? variables.city : " and your city"
+  }  </h3>
+  <ul class="${variables.socialMediaPosition || "position-left"}">
+          <li><a href="${variables.twitter ||
+            "https://twitter.com/4geeksacademy"}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${variables.github ||
+              "https://github.com/4geeksacademy"}"><i class="fab fa-github"></i></a></li>
+            <li><a href="${variables.linkedin ||
+              "https://linkedin.com/school/4geeksacademy"}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=" ${variables.instagram ||
+              "https://instagram.com/4geeksacademy"}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +62,9 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background: "https://picsum.photos/200/300",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://randomuser.me/api/portraits/lego/7.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
